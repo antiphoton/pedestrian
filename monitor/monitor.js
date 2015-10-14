5f3fb6ac4d812546af5221cf2263da639e4ac7fe
cbx <caoboxiao@gmail.com>
2015-10-13 22:15:55 -0400

    WebGl

--- monitor/monitor.js
+++ monitor/monitor.js
@@ -14,6 +14,7 @@
 				frame['position']=parseFloat64(frame['position']);
 				frame['velocity']=parseFloat64(frame['velocity']);
 				frame['acceleration']=parseFloat64(frame['acceleration']);
+				frame['destGate']=parseByte(frame['destGate']);
 			});
 		}
 	})();
@@ -29,13 +30,14 @@
 			}
 		}
 	);
-	var svg;
-	var people=[];
 	var maxPeople,totalFrame;
-	var createSvg=(function() {
+	var peopleColor=['0000ff','ff0000'];
+	var svgRendering=(function() {
+		var svg;
 		var container;
 		var playgroundWidth,playgroundHeight;
 		var windowWidth,windowHeight;
+		var people=[];
 		var onResize=function() {
 			var w=window['innerWidth'];
 			var h=window['innerHeight'];
@@ -55,7 +57,7 @@
 				svg['attr']('transform','matrix('+sH+',0,0,-'+sH+','+windowWidth*(1-sH/sW)/2+','+windowHeight+')');
 			}
 		};
-		return function() {
+		var init=function() {
 			container=d3['select']('body')['append']('svg');
 			svg=container['append']('g');
 			playgroundWidth=data['playground']['width'];
@@ -69,38 +71,141 @@
 				person['attr']('r',0.2);
 				people['push'](person);
 			}
-			interpolateFrame(0);
-		}
-	})();
-	var interpolateFrame=(function() {
-		return function(iFrame) {
-			var i1=Math['floor'](iFrame);
-			var i2=i1+1;
-			var a1=i2-iFrame;
-			var a2=1-a1;
-			var i;
-			var f1=data['frames'][i1];
-			var f2=data['frames'][i2];
-			var x,y,z;
-			for (i=0;i<maxPeople;i++) {
-				x=0;
-				y=0;
-				z=0;
-				if (a1>0&&f1['exist'][i]) {
-					x+=f1['position'][i*2  ]*a1;
-					y+=f1['position'][i*2+1]*a1;
-					z+=a1;
-				}
-				if (a2>0&&f2['exist'][i]) {
-					x+=f2['position'][i*2  ]*a2;
-					y+=f2['position'][i*2+1]*a2;
-					z+=a2;
-				}
-				people[i]['attr']('transform','translate('+x/z+','+y/z+')');
-				people[i]['attr']('opacity',z);
+			renderFrame(0);
+		};
+		var calc=function(personId,visible,x,y,groupId) {
+			if (visible===0) {
+				people[personId]['attr']('display','none');
+				return;
 			}
+			people[personId]['attr']('display','inline');
+			people[personId]['attr']('fill','#'+peopleColor[groupId]);
+			people[personId]['attr']('transform','translate('+x/visible+','+y/visible+')');
+			people[personId]['attr']('opacity',visible);
+		};
+		var render=function() {
+		};
+		return {//svgRendering
+			init:init,
+			calc:calc,
+			render:render
 		};
 	})();
+	var threeRendering=(function() {
+		var canvas;
+		var scene;
+		var camera;
+		var renderer;
+		var playgroundWidth,playgroundHeight;
+		var windowWidth,windowHeight;
+		var people=[];
+		var peopleGeometry=new THREE['CircleGeometry'](0.2);
+		var peopleMaterial=peopleColor['map'](function(x) {
+			return new THREE['LineBasicMaterial']({'color':parseInt(x,16)});
+		});
+		var onResize=function() {
+			var w=window['innerWidth'];
+			var h=window['innerHeight'];
+			if (w===windowWidth&&h===windowHeight) {
+				return ;
+			}
+			windowWidth=w;
+			windowHeight=h;
+			renderer['setSize'](windowWidth,windowHeight);
+			var sW=windowWidth/playgroundWidth;
+			var sH=windowHeight/playgroundHeight;
+			var sM=sW<sH?sW:sH;
+			camera['left']=0;
+			camera['right']=playgroundWidth;
+			camera['bottom']=0;
+			camera['top']=playgroundHeight;
+			if (sW<sH) {
+				camera['bottom']-=(windowHeight/windowWidth*playgroundWidth-playgroundHeight)/2
+				camera['top']+=(windowHeight/windowWidth*playgroundWidth-playgroundHeight)/2
+			}
+			else {
+				camera['left']-=(windowWidth/windowHeight*playgroundHeight-playgroundWidth)/2
+				camera['right']+=(windowWidth/windowHeight*playgroundHeight-playgroundWidth)/2
+			}
+			camera['updateProjectionMatrix']();
+		};
+		var init=function() {
+			renderer=new THREE['WebGLRenderer']();
+			renderer['setClearColor'](0xffffff);
+			document['body']['appendChild'](renderer['domElement']);
+			canvas=d3['select']('canvas');
+			camera=new THREE['OrthographicCamera'](-1,1,1,-1,0.1,1000);
+			scene=new THREE['Scene']();
+			camera['position']['z']=10;
+			camera['up']['set'](0,1,0);
+			playgroundWidth=data['playground']['width'];
+			playgroundHeight=data['playground']['height'];
+			onResize();
+			window['onresize']=onResize;
+			var background=new THREE['Mesh'](new THREE['BoxGeometry'](playgroundWidth,playgroundHeight,0.1),new THREE['LineBasicMaterial']({'color':0xefefef}));
+			background['position']['set'](playgroundWidth/2,playgroundHeight/2,-1);
+			scene['add'](background);
+			var i;
+			var person;
+			for (i=0;i<maxPeople;i++) {
+				person=new THREE['Mesh'](peopleGeometry,peopleMaterial[0]);
+				scene['add'](person);
+				people['push'](person);
+			}
+			renderFrame(0);
+		};
+		var calc=function(personId,visible,x,y,groupId) {
+			var person=people[personId];
+			if (visible===0) {
+				person['visible']=false;
+				return ;
+			}
+			person['visible']=true;
+			person['scale']['set'](visible,visible,visible);
+			person['position']['x']=x/visible;
+			person['position']['y']=y/visible;
+			person['material']=peopleMaterial[groupId];
+		};
+		var render=function() {
+			renderer['render'](scene,camera);
+		};
+		return {//threeRendering
+			init:init,
+			calc:calc,
+			render:render
+		};
+	})();
+	var rendering=threeRendering;
+	var renderFrame=function(iFrame) {
+		var i1=Math['floor'](iFrame);
+		var i2=i1+1;
+		var a1=i2-iFrame;
+		var a2=1-a1;
+		var i;
+		var f1=data['frames'][i1];
+		var f2=data['frames'][i2];
+		var x,y,z;
+		var gateId;
+		for (i=0;i<maxPeople;i++) {
+			x=0;
+			y=0;
+			z=0;
+			if (a1>0&&f1['exist'][i]) {
+				x+=f1['position'][i*2  ]*a1;
+				y+=f1['position'][i*2+1]*a1;
+				gateId=f1['destGate'][i];
+				z+=a1;
+			}
+			if (a2>0&&f2['exist'][i]) {
+				x+=f2['position'][i*2  ]*a2;
+				y+=f2['position'][i*2+1]*a2;
+				gateId=f2['destGate'][i];
+				z+=a2;
+			}
+			rendering.calc(i,z,x,y,gateId);
+		}
+		rendering.render();
+	};
 	var createDraggable=function() {
 		var obj=d3['select']('body')['append']('span');
 		var xObj=0,yObj=0;
@@ -177,7 +282,7 @@
 				currentFrameF=totalFrame-1;
 			}
 			txtIndex['text'](Math['floor'](currentFrameF)+'/'+totalFrame);
-			interpolateFrame(currentFrameF);
+			renderFrame(currentFrameF);
 		};
 		var render=function() {
 			if (!playing) {
@@ -271,7 +376,7 @@
 		var total=2;
 		var finished=0;
 		var main=function() {
-			createSvg();
+			rendering.init();
 			initControl();
 		};
 		return function() {
